b147e2ebb7211f35b1241188bd5fb3f2f1dbb75b
chvndler <70621588+chvndler@users.noreply.github.com>
2021-09-08 18:57:24 -0400

    Home Change

--- pages/index.js
+++ pages/index.js
@@ -12,62 +12,54 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <Link href="/privacy">
-        <a>Privacy Policy</a>
+      <div className={styles.header}>
+        <Link href="/home">
+        <a className={styles.svg}>
+          <svg className={styles.svg} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 275.08">
+            <rect width="300" height="275.08" fill="none" />
+            <polygon points="67.25 22.62 111.73 61.96 13.32 194.6 143.97 175.56 212.02 243.54 268.23 22.62 67.25 22.62" fill="#000" />
+            <path d="M249.61,196.58a4.35,4.35,0,0,0,.76-2.63,4,4,0,0,0-1.35-3.22,5.65,5.65,0,0,0-3.79-1.16h-7v13.76h3.16v-5h2.7l3,5h3.62l-3.32-5.27A4,4,0,0,0,249.61,196.58Zm-3.11-1.1a2.83,2.83,0,0,1-1.86.54h-3.26v-4.08h3.26a2.83,2.83,0,0,1,1.86.54,1.81,1.81,0,0,1,.64,1.47A1.91,1.91,0,0,1,246.5,195.48Z"
+            fill="#000" />
+            <path d="M250.57,185.81a13.27,13.27,0,0,0-12.78,0,12.47,12.47,0,0,0-4.64,4.48,12,12,0,0,0,0,12.32,12.45,12.45,0,0,0,4.64,4.47,13.2,13.2,0,0,0,12.78,0,12.45,12.45,0,0,0,4.64-4.47,12,12,0,0,0,0-12.32A12.47,12.47,0,0,0,250.57,185.81Zm2.78,15.76a10.75,10.75,0,0,1-18.34,0,9.88,9.88,0,0,1,0-10.24,10.45,10.45,0,0,1,3.87-3.74,10.87,10.87,0,0,1,10.6,0,10.45,10.45,0,0,1,3.87,3.74,9.88,9.88,0,0,1,0,10.24Z"
+            fill="#000" />
+          </svg>
+        </a>
         </Link>
+      </div>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+      <div className={styles.sidenav}>
+        <a href="https://hldtru.us/collections/new" target="_blank" rel="noreferrer">NEW</a>
+        <a href="https://hldtru.us/collections/shorts" target="_blank" rel="noreferrer">SHORTS</a>
+        <a href="https://hldtru.us/collections/tees" target="_blank" rel="noreferrer">TEES</a>
+        <a href="https://hldtru.us/collections/all" target="_blank" rel="noreferrer">SHOP ALL</a>
+        <br />
+        <Link href="/">
+        <a>
+          <h5>Home</h5>
+        </a>
+        </Link>
+        <Link href="/cover">
+        <a>
+          <h5>Cover</h5>
+        </a>
+        </Link>
+      </div>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+
+
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/holdtrue.svg" alt="Vercel Logo" width={82} height={18} />
-          </span>
-        </a>
+      <Link href="/">
+      <a>Contact</a>
+      </Link>
+
+      <Link href="/">
+      <a>Terms & Conditions</a>
+      </Link>
+
+      <a href="https://instagram.com/hldtru" target="_blank" rel="noreferrer">Instagram</a>
       </footer>
     </div>
   )
