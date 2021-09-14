4d857c8d5554a3dd673ed20b3a6ec80af3a47750
chvndler <70621588+chvndler@users.noreply.github.com>
2021-09-14 16:59:55 -0400

    Update subscribe.js

--- pages/subscribe.js
+++ pages/subscribe.js
@@ -1,34 +1,66 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
-import Background from '../public/background.jpg'
+import Script from 'next/script'
+import { ChakraProvider } from "@chakra-ui/react"
+import { InputGroup, Code, Heading, Text, Input, InputRightElement, Button, Container, Stack } from "@chakra-ui/react"
+import { Center, Square, Circle } from "@chakra-ui/react"
 import styles from '../styles/Subscribe.module.css'
-import InputGroup from 'react-bootstrap/InputGroup';
-import FormControl from 'react-bootstrap/FormControl';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+
 
 export default function Home() {
   return (
 
-<div className={styles.div}>
-			<Container fluid="sm">
-			  <Row>
-			    <Col>1 of 1
+		<div className={styles.container}>
+			<Head>
+				<title>hldtru®</title>
+				<meta name="description" content="Web Application for hldtru®" />
+				<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+				<link rel="icon" href="/favicon.ico" />
+				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+				<link rel="manifest" href="/site.webmanifest" />
+			</Head>
 
-			<InputGroup hasValidation size="sm" className="mb-3">
-	    <InputGroup.Text id="inputGroup-sizing-sm">Subscribe</InputGroup.Text>
-	    <FormControl aria-label="Small" aria-describedby="inputGroup-sizing-sm" />
-	  </InputGroup>
+		<div className={styles.container}>
+	      <Script
+				  src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=Public API Key"
+					strategy="beforeInteractive"
+				/>
 
-			<br />
-			<h1>SUBSCRIBE</h1>
 
-			</Col>
-	 </Row>
- </Container>
 
+
+			<ChakraProvider>
+			      <div className={styles.container_form}>
+			        <div className={styles.center}>
+			          <Container maxW="xl" centerContent>
+			            <Heading as="h2" size="xl">
+			              Subscribe.
+			            </Heading>
+			            <br />
+                  <div className="klaviyo-form-WfEhm2"></div>
+			            <br />
+			            <br />
+			            <Text fontSize="sm">
+			              <i>
+			                power in <strong>truth</strong>
+			              </i>
+			              ®
+			            </Text>
+			          </Container>
+			        </div>
+			      </div>
+						</ChakraProvider>
+
+
+
+
+
+
+
+</div>
 </div>
 
   )
