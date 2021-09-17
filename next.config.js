e461a3aa381aa2b17f0812389c22118d4d4c1de7
chvndler <70621588+chvndler@users.noreply.github.com>
2021-09-17 13:11:47 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   reactStrictMode: true,
-	assetPrefix: isProd ? 'https://cdn.hldtru.space' : '',
+	assetPrefix: isProd ? 'https://cdn.hldtru.co' : '',
 	images: {
    domains: ['cdn.hldtru.space'],
   }
