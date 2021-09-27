d5c2eef7542640aaba9f0e920c18f44cdb8e5d29
chvndler <70621588+chvndler@users.noreply.github.com>
2021-09-27 19:23:30 -0400

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -26,67 +26,62 @@ export default function Contact() {
   return (
 
 <div className={styles.canvas_fluid}>
+
   <Head>
     <title>About.</title>
     <meta name="description" content="Web Application for hldtru®" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, user-scalable=no"
-    />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
     <link rel="icon" href="/favicon.ico" />
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
     <link rel="manifest" href="/site.webmanifest" />
-    <link
-      rel="stylesheet"
-      href="https://cdn.hldtru.io/stylesheets/font.css"
-    />
+    <link rel="stylesheet" href="https://cdn.hldtru.io/stylesheets/font.css" />
   </Head>
 
-<ChakraProvider>
+  <ChakraProvider>
 
-<Nav />
+    <Nav />
 
-  <Container pt="200" maxW="container.md">
-  <Heading as="h4" size="lg">
-    Contact us.
-  </Heading>
-  <Skeleton height="12px" isLoaded />
+    <Container pt="200" maxW="container.md">
+      <Heading as="h4" size="lg">
+        Contact us.
+      </Heading>
+      <Skeleton height="12px" isLoaded />
 
-    <Text fontSize="sm">
-      <strong>For wholesale inquiries, please contact:</strong>
-    </Text>
+      <Text fontSize="sm">
+        <strong>For wholesale inquiries, please contact:</strong>
+      </Text>
 
-    <Text fontSize="sm">
-    <Link href="mailto:wholesale@hldtru.com" target="_blank" rel="noreferrer" passHref>
-      <a>wholesale@hldtru.com</a>
-    </Link>
-    </Text>
+      <Text fontSize="sm">
+        <Link href="mailto:wholesale@hldtru.com" target="_blank" rel="noreferrer" passHref>
+        <a>wholesale@hldtru.com</a>
+        </Link>
+      </Text>
 
-    <Skeleton height="22px" isLoaded />
+      <Skeleton height="22px" isLoaded />
 
-    <Text fontSize="sm">
-      <strong>For press inquiries, please contact:</strong>
-    </Text>
-    <Text fontSize="sm">
-    <Link href="mailto:media@hldtru.com" target="_blank" rel="noreferrer" passHref>
-      <a>media@hldtru.com</a>
-    </Link>
-    </Text>
+      <Text fontSize="sm">
+        <strong>For press inquiries, please contact:</strong>
+      </Text>
+      <Text fontSize="sm">
+        <Link href="mailto:media@hldtru.com" target="_blank" rel="noreferrer" passHref>
+        <a>media@hldtru.com</a>
+        </Link>
+      </Text>
 
-    <Skeleton height="22px" isLoaded />
+      <Skeleton height="22px" isLoaded />
 
-    <Text fontSize="sm">
-      <strong>For general inquiries, please contact:</strong>
-    </Text>
-    <Text fontSize="sm">
-    <Link href="mailto:shop@hldtru.com" target="_blank" rel="noreferrer" passHref>
-      <a>shop@hldtru.com</a>
-    </Link>
-    </Text>
+      <Text fontSize="sm">
+        <strong>For general inquiries, please contact:</strong>
+      </Text>
+      <Text fontSize="sm">
+        <Link href="mailto:shop@hldtru.com" target="_blank" rel="noreferrer" passHref>
+        <a>shop@hldtru.com</a>
+        </Link>
+      </Text>
 
-<Skeleton height="40px" isLoaded />
+      <Skeleton height="40px" isLoaded />
 
       <Text fontSize="sm">
         <strong>instagram: </strong>
@@ -111,9 +106,9 @@ export default function Contact() {
 
 
 
-  </Container>
- </ChakraProvider>
- <Socialfooter />
+    </Container>
+  </ChakraProvider>
+  <Socialfooter />
 </div>
  )
 }
