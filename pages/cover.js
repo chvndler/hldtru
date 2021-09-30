8e3a74acf389b6a5ec0a9b496aed283085676fe3
chvndler <70621588+chvndler@users.noreply.github.com>
2021-09-30 16:13:34 -0400

    Update cover.js

--- pages/cover.js
+++ pages/cover.js
@@ -9,30 +9,38 @@ import { ChakraProvider, Center, Box, } from "@chakra-ui/react"
 export default function Cover() {
   const ref = useRef()
   return (
-  <div className={styles.canvas}>
-    <Head>
-     <title>Cover.</title>
-			 <meta name="description" content="Web Application for hldtru®" />
-			 <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-			 <link rel="icon" href="/favicon.ico" />
-			 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-			 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-			 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-			 <link rel="manifest" href="/site.webmanifest" />
-    </Head>
+		
+<div className={styles.canvas}>
+  <Head>
+    <title>Cover.</title>
+    <meta name="description" content="Web Application for hldtru®" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, user-scalable=no"
+    />
+    <link rel="icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <link rel="manifest" href="/site.webmanifest" />
+  </Head>
 
+  <Canvas shadows dpr={[1, 2]} camera={{ fov: 90 }}>
+    <Suspense fallback={null}>
+      <Stage
+        controls={ref}
+        preset="rembrandt"
+        intensity={2}
+        contactShadow={false}
+        environment="forest"
+      >
+        <Layout />
+      </Stage>
+    </Suspense>
+    <OrbitControls ref={ref} />
+  </Canvas>
+</div>
 
-    <Canvas shadows dpr={[1, 2]} camera={{ fov: 90 }}>
-      <Suspense fallback={null}>
-        <Stage controls={ref} preset="rembrandt" intensity={2}  contactShadow={false} environment="forest">
-          <Layout />
-        </Stage>
-      </Suspense>
-      <OrbitControls ref={ref} />
-    </Canvas>
-
-
-  </div>
 
   )
 }
